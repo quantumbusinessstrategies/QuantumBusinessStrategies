f45dcd02467d82845c78a734b29389c10bd6835b
quantumbusinessstrategies <quantumbusinessstrategies@gmail.com>
2025-12-04 18:16:05 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
-
-
-createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<App />
-</React.StrictMode>
-)
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Quantum Business Strategies</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
